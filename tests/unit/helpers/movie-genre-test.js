10198e7d5c2c9fafd2774c2738340ba53fc9dbdf
vidaaudrey <audrey@33road.com>
2016-02-26 13:29:05 -0800

    feat: movie-list

---
+++ tests/unit/helpers/movie-genre-test.js
@@ -0,0 +1,10 @@
+import { movieGenre } from 'viedor/helpers/movie-genre';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | movie genre');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = movieGenre([42]);
+  assert.ok(result);
+});
